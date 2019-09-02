5c4ea03703af6f4975f482d64f811049e9494bb9
Lucas Farias <lukf95@gmail.com>
2019-09-01 21:55:41 -0300

    Asd

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,15 @@ import Home from './shared/views/home';
 const server = express();
 const port = 8080;
 
-server.use(express.static('/static'));
+server.use('/static', express.static(path.join(__dirname, 'build/client')));
 
 server.get('/', (req, res) => {
   const view = ReactDOMServer.renderToString(React.createElement(Home, null, null));
   res.send(template(view));
+});
+
+server.use((req, res) => {
+  res.send('Error 404: Page not found');
 });
 
 server.listen(port, () => {
